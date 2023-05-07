ea1eb3b258580ab789bf577228d8fa31bb015e5b
Manu Hegde <manu.hegde02@gmail.com>
2023-05-07 15:03:48 -0400

    Refined collision detection (works properly with interactions). Integrating MPU6050, now particles respond accordingly to orientation of matrix as it is rotated and angled. Some  bugs emerged from this new software, listed in comments

--- main/main.ino
+++ main/main.ino
@@ -6,14 +6,17 @@
 #define PIN 12
 #define BUTTON 0
 
-#define NUM_OF_BALLS 15
+#define NUM_OF_BALLS 30
 #define NUM_OF_LEDS 256
+#define HEIGHT 16
+#define WIDTH 16
 
 Adafruit_NeoPixel pixel = Adafruit_NeoPixel(NUM_OF_LEDS, PIN, NEO_GRB+NEO_KHZ800);
 #define COLOR = pixel.Color(150, 0, 0)
 
 Adafruit_MPU6050 mpu;
 float accelX = 0, accelY = 0, accelZ = 0, gyroX = 0, gyroY = 0, gyroZ = 0, gyroXDiff = 0, gyroYDiff = 0, gyroZDiff = 0, accXDiff = 0, accYDiff = 0, accZDiff = 0;
+int matrix[HEIGHT * WIDTH];
 
 Adafruit_SSD1306 lcd(128, 64); // create display object
 
@@ -21,14 +24,14 @@ class Ball {
   private:
     int posx;
     int posy;
-    int xRate;
-    int yRate;
+    float xRate;
+    float yRate;
     uint32_t color;
 
   public:
     Ball() {}
 
-    Ball(int x, int y, int mx, int my, uint32_t c) {
+    Ball(int x, int y, float mx, float my, uint32_t c) {
       posx = x;
       posy = y;
       xRate = mx;
@@ -95,13 +98,21 @@ SquareObstacle obstacles[9] = {
 
 Ball balls[NUM_OF_BALLS]; 
 
+int getIndex(int x, int y) {
+  int curX = (y % 2 == 0) ? x : 15 - x;
+  return curX + y*16;
+}
+
 // Combine create functions?
 void createBalls() {
   for (int i = 0; i < NUM_OF_BALLS; i++) {
     int xRate = 0, yRate = 0;
     while (xRate == 0) xRate = random(-1, 1);
     while (yRate == 0) yRate = random(-1, 1);
-    balls[i] = Ball(random(0, 16), random(0, 16), xRate, yRate, pixel.Color(random(0, 100), random(0, 100), random(0, 100)));
+    int x = random(0, 16);
+    int y = random(0, 16);
+    matrix[getIndex(x, y)] = 1;
+    balls[i] = Ball(x, y, xRate, yRate, pixel.Color(random(0, 100), random(0, 100), random(0, 100)));
   }
 }
 
@@ -113,6 +124,7 @@ void createObstacles() {
   int index = 0;
   for (int i = 0; i < 3; i++) {
     for (int j = 0; j < 3; j++) {
+      matrix[getIndex(x, y)] = 1;
       obstacles[index] = SquareObstacle(x, y, pixel.Color(100, 100, 100), 3);
       x++;
       index++;
@@ -123,7 +135,7 @@ void createObstacles() {
 }
 
 void calibrationFunc() {
-  int rounds = 500;
+  int rounds = 250;
   // Potentially have progress bar???
   lcd.print("Calibrating accelerometer and gyroscope . . . ");
   lcd.display();
@@ -144,7 +156,7 @@ void calibrationFunc() {
   gyroZDiff = gZ/rounds;
   accXDiff = aX/rounds;
   accYDiff = aY/rounds;
-  accZDiff = aZ/rounds; // subtract by 9.8 if I want to normalize
+  accZDiff = aZ/rounds - 9.80;
 
   lcd.clearDisplay();
   lcd.setCursor(0, 0);
@@ -156,13 +168,20 @@ void calibrationFunc() {
   lcd.display();
 }
 
+void drawPixel(Ball& ball) {
+  int index = getIndex(ball.getX(), ball.getY());
+  pixel.setPixelColor(index, ball.getColor());
+}
+
 void setup() {
-  pixel.setBrightness(75);
+  pixel.setBrightness(50);
   createBalls();
   createObstacles();
   pixel.begin();
   Serial.begin(9600);
   pinMode(BUTTON, INPUT_PULLUP);
+
+  
 
   if (!mpu.begin()) {
     Serial.println("Failed to find MPU6050 chip");
@@ -182,39 +201,100 @@ void setup() {
   lcd.setTextSize(1);
   lcd.setCursor(0, 0);
 
-  calibrationFunc();
-}
-
-int getIndex(int x, int y) {
-  int curX = (y % 2 == 0) ? x : 15 - x;
-  return curX + y*16;
-}
-
-// May need more efficient method once pixel count grows
-bool occupiedPixel(int x, int y) {
   for (int i = 0; i < NUM_OF_BALLS; i++) {
-    if (balls[i].getX() == x && balls[i].getY() == y) return true;
+    drawPixel(balls[i]);
   }
-  return false;
+
+  calibrationFunc();
 }
 
 bool withinBounds(int x, int y) {
   return x >= 0 && x <= 15 && y >= 0 && y <= 15 ? true : false;
 }
 
-void checkCollisions(int x, int y, Ball& ball) {
-  if (occupiedPixel(x - ball.getXRate(), y - ball.getYRate())) {ball.setXRate(ball.getXRate() * -1); ball.setYRate(ball.getYRate() * -1);}
-  else {
-    if (x == 0 || x == 15) ball.setXRate(ball.getXRate() * -1);
-    if (y == 0 || y == 15) ball.setYRate(ball.getYRate() * -1);
-    for (int i = 0; i < 9; i++) {
-      if (x == obstacles[i].getX() && y == obstacles[i].getY()) {
-        ball.setXRate(ball.getXRate() * -1);
-        ball.setYRate(ball.getYRate() * -1);
-        break;
+
+void checkCollisions(Ball& ball) {
+  int x = ball.getX();
+  int y = ball.getY();
+  int newX = x + ball.getXRate();
+  int newY = y + ball.getYRate();
+
+  // Edge detection finally works
+
+  if (newX > 15) {
+    ball.setXRate(ball.getXRate() / -2);
+    newX = 15;
+  } else if (newX < 0) {
+    ball.setXRate(ball.getXRate() / -2);
+    newX = 0;
+  }
+
+  if (newY > 15) {
+    ball.setYRate(ball.getYRate() / -2);
+    newY = 15;
+  } else if (newY < 0) {
+    ball.setYRate(ball.getYRate() / -2);
+    newY = 0;
+  }
+
+  int oldIndex = getIndex(x, y);
+  int newIndex = getIndex(newX, newY);
+
+  if (oldIndex != newIndex && matrix[newIndex]) {
+    int delta = abs(newIndex - oldIndex);
+      if (delta == 1) {
+        newX = x;
+        ball.setXRate(ball.getXRate() / -2);
+        newIndex = oldIndex;
+      }
+      else if (abs(ball.getXRate()) >= abs(ball.getYRate())) {
+        newIndex = getIndex(newX, y);
+        if (!matrix[newIndex]) {
+          ball.setYRate(ball.getYRate() / -2);
+          newY = y;
+        } else {
+          newIndex = getIndex(x, newY);
+          if (!matrix[newIndex]) {
+            ball.setXRate(ball.getXRate() / -2);
+            newX = x;
+          } else {
+            newX = x;
+            newY = y;
+            ball.setXRate(ball.getXRate() / -2);
+            ball.setYRate(ball.getYRate() / -2);
+            newIndex = oldIndex;
+          }
+        }
+      } else {
+        newIndex = getIndex(x, newY);
+        if (!matrix[newIndex]) {
+            ball.setXRate(ball.getXRate() / -2);
+            newX = x; 
+        } else {
+          newIndex = getIndex(newX, y);
+          if (!matrix[newIndex]) {
+            ball.setYRate(ball.getYRate() / -2);
+            newY = y;
+          } else { 
+            newX = x;
+            newY = y;
+            ball.setXRate(ball.getXRate() / -2);
+            ball.setYRate(ball.getYRate() / -2);
+            newIndex = oldIndex;
+          }
+        }
       }
     }
-  }
+  
+  // Think about:
+  // When updated, the states change so won't be updated correctly
+  // Red ball gets changed, so green thinks it can move forward
+  // So pause for one tick, so all directions are updated, then allow them to move
+
+  matrix[oldIndex] = 0; 
+  matrix[newIndex] = 1;
+  ball.setX(newX);
+  ball.setY(newY);
 }
 
 void accelGyroData() {
@@ -238,6 +318,37 @@ void accelGyroData() {
   Serial.print("x: "); Serial.println(gyroX);
   Serial.print("y: "); Serial.println(gyroY);
   Serial.print("z: "); Serial.println(gyroZ);
+
+  // Resting: 0, 0, 9.8
+  // Left: 0, -9.8, 0
+  // Right: 0, 9.8, 0
+  // Up: -9.8, 0, 0
+  // Down: 9.8, 0, 0
+  // Flipped: 0, 0, -9.8
+
+  int16_t ax = accelX / 1.225;
+  int16_t ay = accelY / 1.225;
+  int16_t az = accelZ / 4.9;
+
+  float mX = ay;                 
+  float mY = -az;
+  mY -= ax / 2;
+
+  Serial.print(mX);
+  Serial.print(", ");
+  Serial.print(mY);
+  Serial.print(", ");
+  Serial.println(az);
+
+  int16_t az2 = az * 2 + 1;
+
+  if (mX != 0) mX /= abs(mX);
+  if (mY != 0) mY /= abs(mY);
+
+  for (int i = 0; i < NUM_OF_BALLS; i++) {
+    balls[i].setXRate(balls[i].getXRate() + mX);
+    balls[i].setYRate(balls[i].getYRate() + mY);
+  }
 }
 
 byte prev = 1;
@@ -250,12 +361,8 @@ void loop() {
     accelGyroData();
     pixel.clear();
     for (int i = 0; i < NUM_OF_BALLS; i++) {
-      int pixelNum = getIndex(balls[i].getX(), balls[i].getY());
-      pixel.setPixelColor(pixelNum, balls[i].getColor());
-      checkCollisions(balls[i].getX(), balls[i].getY(), balls[i]);
-      int newX = balls[i].getX() - balls[i].getXRate();
-      int newY = balls[i].getY() - balls[i].getYRate();
-      if (withinBounds(newX, newY) && !occupiedPixel(newX, newY)) {balls[i].setX(newX); balls[i].setY(newY);}
+      checkCollisions(balls[i]);
+      drawPixel(balls[i]);
     }
 
     for (int i = 0; i < 9; i++) {
@@ -264,7 +371,14 @@ void loop() {
     }
 
     pixel.show();
-    delay(100);
+    delay(125);
   }
   prev = cur;
 }
+
+// Bugs:
+// Weird edge detection with x = 15 column - always read as occupied
+// Weird edge detection with y-values - when turned sideways, particles alternate bc each gap is read as occupied
+// Improve math for gravity and movement
+  // Specifically add randomness, like in library, so stacks can tumble rather than creating blockage
+// Continue improving collisions
